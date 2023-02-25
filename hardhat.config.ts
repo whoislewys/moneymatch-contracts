543c605f0ef2b04b3a5aa21ad7a1b7113c5c7dde
whoislewys <whoislewys@gmail.com>
2023-02-25 09:41:12 -0700

    🇮 mumbai network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
     hardhat: {
       chainId: 1337,
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY ?? '0x0'],
     },
   },
 };
